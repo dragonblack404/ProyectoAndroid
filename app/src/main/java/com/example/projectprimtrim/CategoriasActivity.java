dbf7fecbf0a64c2c7662cd25200ad4119bfafc13
Dragon Black <mdbetico.96@gmail.com>
2022-12-11 07:10:11 +0000

    commit 3
    Terminado la implementación de los menus y toast

--- app/src/main/java/com/example/projectprimtrim/CategoriasActivity.java
+++ app/src/main/java/com/example/projectprimtrim/CategoriasActivity.java
@@ -1,8 +1,12 @@
 package com.example.projectprimtrim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class CategoriasActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class CategoriasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorias);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.items_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent cambiarActividad;
+        if (item != null){
+            cambiarActividad = new Intent(this, MainActivity.class);
+            startActivity(cambiarActividad);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
--- app/src/main/java/com/example/projectprimtrim/MainActivity.java
+++ app/src/main/java/com/example/projectprimtrim/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.projectprimtrim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+/*
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Asociamos el
+    }
+*/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.inicio_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent cambiarActividad;
+
+        switch (item.getItemId()){
+            case R.id.acercaItem:
+                Toast.makeText(this, "Acerca de..", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.homeItem:
+                Toast.makeText(this, "Inicio", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.catItem:
+                Toast.makeText(this, "Categorias", Toast.LENGTH_SHORT).show();
+                cambiarActividad = new Intent(this, CategoriasActivity.class);
+                startActivity(cambiarActividad);
+                break;
+            case R.id.formItem:
+                Toast.makeText(this, "Formulario", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.listItem:
+                Toast.makeText(this, "Listado", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
